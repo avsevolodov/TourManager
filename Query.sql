246c1675bdb76cca11ec57d662c76b252b1d4901
avsevolodov <avsevolodov@mail.ru>
2016-04-11 11:53:04 +0300

    -sql

---
+++ Query.sql
@@ -0,0 +1,11 @@
+﻿select fs.productid, count(1)
+  FROM (WITH firstSale AS (SELECT s.salesid,
+                                  s.customerid,
+                                  s.productid,
+                                  s.datetime,
+                                  ROW_NUMBER() OVER(PARTITION BY s.customerid ORDER BY s.datetime ASC) AS rk
+                             FROM Sales s)
+         SELECT s.*
+           FROM firstSale s
+          WHERE s.rk = 1) fs
+          GROUP BY fs.productid
